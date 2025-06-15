880b7488e70a1a18490dc907664454be2c2687cf
Federico Herrera <fede.juan.herrera@gmail.com>
2025-06-15 13:49:25 -0300

    Initial Commit

---
+++ AppComidas/AppComidasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppComidasApp.swift
+//  AppComidas
+//
+//  Created by Federico Herrera on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppComidasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
